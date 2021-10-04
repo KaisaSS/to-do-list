63a0dc4a0e2b79f8b95ec4459acfe796ff73462e
Kaisa <parvej@Kaisas-MacBook-Pro.local>
2021-10-04 15:31:18 +0100

    Add button and input area to edit input

--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 
 function ToDoForm(props) {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState(props.edit ? props.edit.value : "");
 
   const inputRef = useRef(null);
 
@@ -26,16 +26,33 @@ function ToDoForm(props) {
 
   return (
     <form className="to-do-form" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Add new item"
-        value={input}
-        name="text"
-        className="to-do-input"
-        onChange={handleChange}
-        ref={inputRef}
-      />
-      <button className="to-do-button">Add New Item</button>
+      {props.edit ? (
+        <>
+          <input
+            type="text"
+            placeholder="Update your item"
+            value={input}
+            name="text"
+            className="to-do-input edit"
+            onChange={handleChange}
+            ref={inputRef}
+          />
+          <button className="to-do-button edit">Update Item</button>
+        </>
+      ) : (
+        <>
+          <input
+            type="text"
+            placeholder="Add new item"
+            value={input}
+            name="text"
+            className="to-do-input"
+            onChange={handleChange}
+            ref={inputRef}
+          />
+          <button className="to-do-button">Add New Item</button>
+        </>
+      )}
     </form>
   );
 }
