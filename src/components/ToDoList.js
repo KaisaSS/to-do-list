2f1f104c04f880263f7184c876ee773cfa495c7e
Kaisa <parvej@Kaisas-MacBook-Pro.local>
2021-10-04 14:34:33 +0100

    Create update, remove, and complete functions for list items

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import ToDoForm from "./ToDoForm";
+import ToDo from "./ToDo";
 
 function ToDoList() {
-  const [toDos, setToDos] = useState("");
+  const [toDos, setToDos] = useState([]);
 
   const addToDo = (toDo) => {
     if (!toDo.text || /^\s*$/.test(toDo.text)) {
@@ -14,10 +15,34 @@ function ToDoList() {
     setToDos(newToDos);
   };
 
+  const updateToDo = (toDoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+    setToDos((prev) => prev.map((item) => (item.id === toDoId ? newValue : item)));
+  };
+
+  const removeToDo = (id) => {
+    const removeArr = [...toDos].filter((toDo) => toDo.id !== id);
+
+    setToDos(removeArr);
+  };
+
+  const completeToDo = (id) => {
+    let updatedToDos = toDos.map((toDo) => {
+      if (toDo.id === id) {
+        toDo.isComplete = !toDo.isComplete;
+      }
+      return toDo;
+    });
+    setToDos(updatedToDos);
+  };
+
   return (
     <div>
       <h1>What is the plan for today?</h1>
       <ToDoForm onSubmit={addToDo} />
+      <ToDo toDos={toDos} completeToDo={completeToDo} removeToDo={removeToDo} updateToDo={updateToDo} />
     </div>
   );
 }
